25a8a28bb8818707fbb2e400fbdc4b2811be4247
Nicola Sereno <nicola.sereno@gmail.com>
2026-03-14 17:25:39 +0100

    feat: configure https and cors headers

--- src/main/java/it/sereno/gtfs/controller/StopTimesController.java
+++ src/main/java/it/sereno/gtfs/controller/StopTimesController.java
@@ -2,6 +2,7 @@ package it.sereno.gtfs.controller;
 
 import it.sereno.gtfs.service.WaitTimeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "https://smart-mobility.homelinuxserver.org")
 @RequiredArgsConstructor
 public class StopTimesController {
 
-	private final WaitTimeService service;
+    private final WaitTimeService service;
 
-	@GetMapping( "/api/wait-times/{stopIdentifier}" )
-	public List<WaitTimeService.WaitTime> getStopTimes( @PathVariable final String stopIdentifier ) {
-		return service.getStopWaitTimes( stopIdentifier );
-	}
+    @GetMapping("/api/wait-times/{stopIdentifier}")
+    public List<WaitTimeService.WaitTime> getStopTimes(@PathVariable final String stopIdentifier) {
+        return service.getStopWaitTimes(stopIdentifier);
+    }
 }
